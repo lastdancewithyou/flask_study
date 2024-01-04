2af92d742ce84441e64b2a4b55efda67af74780e
Gangmin Jeong <114273570+lastdancewithyou@users.noreply.github.com>
2024-01-04 14:58:46 +0900

    Add files via upload

---
+++ read.py
@@ -0,0 +1,49 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+topics = [
+    {'id': 1, 'title': 'html', 'body': 'html is ...'},
+    {'id': 2, 'title': 'css', 'body': 'css is ...'},
+    {'id': 3, 'title': 'javascript', 'body': 'javascript is ...'}
+]
+
+def template(contents, content) :
+    return f'''<!doctype html>
+    <html>
+        <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+                {contents}
+            </ol>
+            {content}
+        </body>
+    </html>
+    '''
+def getContents() : 
+    liTags = ''
+    for topic in topics:
+        liTags = liTags + f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
+    return liTags
+
+@app.route('/')
+def index():
+    liTags = ''
+    return template(getContents(), '<h2>Welcome</h2>Hello, WEB')
+
+@app.route('/create/')
+def create():
+    return 'Create'
+
+@app.route('/read/<int:id>/')
+def read(id):
+    liTags = ''
+    title = ''    
+    body=''
+    for topic in topics : 
+        if id == topic['id'] : 
+            title = topic['title']
+            body = topic['body']
+            break
+    return template(getContents(), f'<h2>{title}</h2>{body}')
+app.run(port=5001, debug=True)
